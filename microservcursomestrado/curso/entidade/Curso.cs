75f26642ebb3a45f4e802fe306e42c1212e2e62c
VieiraLucasDuarte <108312353+VieiraLucasDuarte@users.noreply.github.com>
2024-10-03 00:28:22 +0000

    First Commit

---
+++ microservcursomestrado/curso/entidade/Curso.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace microservcursomestrado.curso.entidade
+{
+    public class Curso
+    {
+        public Guid Id { get; set; }
+    }
+}
